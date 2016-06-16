288ffb40aac857cd948baddef481675dda8245b8
bfemmer <bfemmer@gmail.com>
2016-06-16 14:33:16 -0400

    Added MPAndroidChart dependency and updated Dashboard fragment to display dummy data for visualization.

--- mobile/src/main/java/com/bfemmer/portdawgtoolkit/DashboardFragment.java
+++ mobile/src/main/java/com/bfemmer/portdawgtoolkit/DashboardFragment.java
@@ -1,12 +1,27 @@
 package com.bfemmer.portdawgtoolkit;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -26,6 +41,7 @@ public class DashboardFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private RadarChart chart;
 
     private OnFragmentInteractionListener mListener;
 
@@ -63,8 +79,36 @@ public class DashboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        chart = (RadarChart) getView().findViewById(R.id.chart);
+
+
+        RadarData radarData = getRadarData();
+        chart.setData(radarData);
+        chart.getYAxis().setDrawLabels(false);
+        chart.setDescription("Readiness");
+        chart.animateXY(2000, 2000);
+        chart.invalidate();
+
+//        BarData data = new BarData(getXAxisValues(), getDataSet());
+//        chart.setData(data);
+//        chart.setDescription("Readiness");
+//        chart.animateXY(2000, 2000);
+//        chart.invalidate();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        chart = null;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -104,5 +148,207 @@ public class DashboardFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private RadarData getRadarData() {
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("F2F");
+        labels.add("EPR");
+        labels.add("IMR");
+        labels.add("TFAT");
+        labels.add("UGR");
+        labels.add("UOO");
+        labels.add("Veh.");
+        labels.add("483");
+
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(80, 0));
+        entries.add(new Entry(100, 1));
+        entries.add(new Entry(100, 2));
+        entries.add(new Entry(100, 3));
+        entries.add(new Entry(100, 4));
+        entries.add(new Entry(100, 5));
+        entries.add(new Entry(20, 6));
+        entries.add(new Entry(60, 7));
+
+        ArrayList<Entry> entries2 = new ArrayList<>();
+        entries2.add(new Entry(60, 0));
+        entries2.add(new Entry(100, 1));
+        entries2.add(new Entry(80, 2));
+        entries2.add(new Entry(100, 3));
+        entries2.add(new Entry(100, 4));
+        entries2.add(new Entry(60, 5));
+        entries2.add(new Entry(100, 6));
+        entries2.add(new Entry(0, 7));
+
+        ArrayList<Entry> entries3 = new ArrayList<>();
+        entries3.add(new Entry(100, 0));
+        entries3.add(new Entry(80, 1));
+        entries3.add(new Entry(80, 2));
+        entries3.add(new Entry(80, 3));
+        entries3.add(new Entry(80, 4));
+        entries3.add(new Entry(60, 5));
+        entries3.add(new Entry(100, 6));
+        entries3.add(new Entry(50, 7));
+
+        ArrayList<Entry> entries4 = new ArrayList<>();
+        entries4.add(new Entry(70, 0));
+        entries4.add(new Entry(85, 1));
+        entries4.add(new Entry(85, 2));
+        entries4.add(new Entry(85, 3));
+        entries4.add(new Entry(100, 4));
+        entries4.add(new Entry(100, 5));
+        entries4.add(new Entry(75, 6));
+        entries4.add(new Entry(75, 7));
+
+        ArrayList<Entry> entries5 = new ArrayList<>();
+        entries5.add(new Entry(65, 0));
+        entries5.add(new Entry(65, 1));
+        entries5.add(new Entry(65, 2));
+        entries5.add(new Entry(65, 3));
+        entries5.add(new Entry(65, 4));
+        entries5.add(new Entry(65, 5));
+        entries5.add(new Entry(65, 6));
+        entries5.add(new Entry(65, 7));
+
+        ArrayList<Entry> entries6 = new ArrayList<>();
+        entries6.add(new Entry(25, 0));
+        entries6.add(new Entry(55, 1));
+        entries6.add(new Entry(25, 2));
+        entries6.add(new Entry(55, 3));
+        entries6.add(new Entry(25, 4));
+        entries6.add(new Entry(55, 5));
+        entries6.add(new Entry(25, 6));
+        entries6.add(new Entry(55, 7));
+
+        ArrayList<Entry> entries7 = new ArrayList<>();
+        entries7.add(new Entry(30, 0));
+        entries7.add(new Entry(30, 1));
+        entries7.add(new Entry(30, 2));
+        entries7.add(new Entry(30, 3));
+        entries7.add(new Entry(30, 4));
+        entries7.add(new Entry(30, 5));
+        entries7.add(new Entry(30, 6));
+        entries7.add(new Entry(30, 7));
+
+//        ArrayList<Entry> entries = new ArrayList<>();
+//        entries.add(new Entry(0.8f, 0));
+//        entries.add(new Entry(1f, 1));
+//        entries.add(new Entry(1f, 2));
+//        entries.add(new Entry(1f, 3));
+//        entries.add(new Entry(1f, 4));
+//        entries.add(new Entry(1f, 5));
+//        entries.add(new Entry(0.2f, 6));
+//        entries.add(new Entry(0.6f, 7));
+//
+//        ArrayList<Entry> entries2 = new ArrayList<>();
+//        entries2.add(new Entry(0.6f, 0));
+//        entries2.add(new Entry(1f, 1));
+//        entries2.add(new Entry(0.8f, 2));
+//        entries2.add(new Entry(1f, 3));
+//        entries2.add(new Entry(1f, 4));
+//        entries2.add(new Entry(0.6f, 5));
+//        entries2.add(new Entry(1f, 6));
+//        entries2.add(new Entry(0f, 7));
+//
+//        ArrayList<Entry> entries3 = new ArrayList<>();
+//        entries3.add(new Entry(1f, 0));
+//        entries3.add(new Entry(0.8f, 1));
+//        entries3.add(new Entry(0.8f, 2));
+//        entries3.add(new Entry(0.8f, 3));
+//        entries3.add(new Entry(0.8f, 4));
+//        entries3.add(new Entry(0.6f, 5));
+//        entries3.add(new Entry(1f, 6));
+//        entries3.add(new Entry(0.5f, 7));
+
+        RadarDataSet radardataset = new RadarDataSet(entries, "Fleet");
+        RadarDataSet cargoradardataset = new RadarDataSet(entries2, "Cargo");
+        RadarDataSet rampradardataset = new RadarDataSet(entries3, "Ramp");
+        RadarDataSet paxradardataset = new RadarDataSet(entries4, "Pax");
+        RadarDataSet atocradardataset = new RadarDataSet(entries5, "ATOC");
+        RadarDataSet lpradardataset = new RadarDataSet(entries6, "L/P");
+        RadarDataSet shradardataset = new RadarDataSet(entries7, "S/H");
+
+        radardataset.setColor(Color.BLUE);
+        cargoradardataset.setColor(Color.RED);
+        rampradardataset.setColor(Color.GREEN);
+        paxradardataset.setColor(Color.YELLOW);
+        atocradardataset.setColor(Color.CYAN);
+        lpradardataset.setColor(Color.MAGENTA);
+        shradardataset.setColor(Color.DKGRAY);
+
+        radardataset.setDrawFilled(false);
+        cargoradardataset.setDrawFilled(false);
+        rampradardataset.setDrawFilled(false);
+        paxradardataset.setDrawFilled(false);
+        atocradardataset.setDrawFilled(false);
+        lpradardataset.setDrawFilled(false);
+        shradardataset.setDrawFilled(false);
+
+        List<RadarDataSet> radarDataSets = new ArrayList<>();
+        radarDataSets.add(radardataset);
+        radarDataSets.add(cargoradardataset);
+        radarDataSets.add(rampradardataset);
+        radarDataSets.add(paxradardataset);
+        radarDataSets.add(atocradardataset);
+        radarDataSets.add(lpradardataset);
+        radarDataSets.add(shradardataset);
+
+        RadarData data = new RadarData(labels, radarDataSets);
+
+        return data;
+    }
+
+    private ArrayList<BarDataSet> getDataSet() {
+        ArrayList<BarDataSet> dataSets = null;
+
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
+        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
+        valueSet1.add(v1e1);
+        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
+        valueSet1.add(v1e2);
+        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
+        valueSet1.add(v1e3);
+        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
+        valueSet1.add(v1e4);
+        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
+        valueSet1.add(v1e5);
+        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
+        valueSet1.add(v1e6);
+
+        ArrayList<BarEntry> valueSet2 = new ArrayList<>();
+        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
+        valueSet2.add(v2e1);
+        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
+        valueSet2.add(v2e2);
+        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
+        valueSet2.add(v2e3);
+        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
+        valueSet2.add(v2e4);
+        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
+        valueSet2.add(v2e5);
+        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
+        valueSet2.add(v2e6);
+
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
+        barDataSet1.setColor(Color.rgb(0, 155, 0));
+        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
+        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        dataSets = new ArrayList<>();
+        dataSets.add(barDataSet1);
+        dataSets.add(barDataSet2);
+        return dataSets;
+    }
+
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> xAxis = new ArrayList<>();
+        xAxis.add("JAN");
+        xAxis.add("FEB");
+        xAxis.add("MAR");
+        xAxis.add("APR");
+        xAxis.add("MAY");
+        xAxis.add("JUN");
+        return xAxis;
     }
 }
